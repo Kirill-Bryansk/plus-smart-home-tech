4b7f2f87642880c3686fe94aa93c2f220f646e66
Kirill-Bryansk <k.mashnov@yandex.ru>
2026-01-29 11:48:16 +1000

    Добавил модуль Analyzer. Постгри

---
+++ telemetry/analyzer/src/main/java/ru/yandex/practicum/telemetry/analyzer/AnalyzerApplication.java
@@ -0,0 +1,4 @@
+package ru.yandex.practicum.telemetry.analyzer;
+
+public class AnalyzerApplication {
+}
---
+++ telemetry/analyzer/src/main/java/ru/yandex/practicum/telemetry/analyzer/config/KafkaConsumerConfig.java
@@ -0,0 +1,4 @@
+package ru.yandex.practicum.telemetry.analyzer.config;
+
+public class KafkaConsumerConfig {
+}
